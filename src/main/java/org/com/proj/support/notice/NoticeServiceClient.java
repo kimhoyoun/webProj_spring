186774574e3d2155b1effaedad49b243ea527db7
kimhoyoun <hoyoun1309@gmail.com>
2022-07-18 23:52:12 +0900

    test

--- src/main/java/org/com/proj/support/notice/NoticeServiceClient.java
+++ src/main/java/org/com/proj/support/notice/NoticeServiceClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+// test
 public class NoticeServiceClient {
 
 	public static void main(String[] args) {
