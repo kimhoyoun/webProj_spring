c7d70accb580f2b91d7b5f33143fb9d873977c38
kimhoyoun <hoyoun1309@gmail.com>
2022-07-20 01:58:16 +0900

    mybatis 조회 완료

--- src/main/java/org/com/proj/support/notice/NoticeController.java
+++ src/main/java/org/com/proj/support/notice/NoticeController.java
@@ -3,12 +3,15 @@ package org.com.proj.support.notice;
 import java.sql.Connection;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.io.ResolverUtil.Test;
+import org.com.proj.support.notice.impl.NoticeDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -24,8 +27,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/support/notice")
 public class NoticeController {
 	
-	@Inject
-	private DataSource dataSource;
+	@Resource(name = "noticeDAO")
+	private NoticeDAO dao;
 	
 	
 //	@RequestMapping(value = "/List", method = RequestMethod.GET)
@@ -35,16 +38,17 @@ public class NoticeController {
 //		model.addAttribute("mainUrl", "support/notice/list" );
 //		return "contents/template";
 //	}
+	
+	
 	@RequestMapping(value = "/List", method = RequestMethod.GET)
-	public String home(@ModelAttribute Test test, Model model) {
+	public String home(Model model) {
 		
-		try {
-			Connection conn = (Connection) dataSource.getConnection();
-			System.out.println("성공" + conn);
-		}catch(Exception e) {
-			System.out.println("실패..");
-			e.printStackTrace();
+		List<NoticeDTO> list = dao.list();
+		
+		for(NoticeDTO dto : list) {
+			System.out.println(dto);
 		}
+		
 		
 		model.addAttribute("mainUrl", "support/notice/list" );
 		return "contents/template";
