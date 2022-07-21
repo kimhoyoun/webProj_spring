68614d9abcee13732509d47bef1151bd1ed909ad
kimhoyoun <hoyoun1309@gmail.com>
2022-07-21 21:24:08 +0900

    mybatis 연동중

--- src/main/java/org/com/proj/support/notice/NoticeService.java
+++ src/main/java/org/com/proj/support/notice/NoticeService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.com.proj.support.notice.NoticeDTO;
 
 public interface NoticeService {
-	
+	//입력
 	void insertNotice(NoticeDTO dto);
-	
+	// 수정
 	void updateNotice(NoticeDTO dto);
-	
+	// 삭제
 	void deleteNotice(NoticeDTO dto);
-	
+	// 조회
 	NoticeDTO detailNotice(String id);
-	
+	// 조회 all
 	List<NoticeDTO> NoticeList(int start, int limit);
 }
