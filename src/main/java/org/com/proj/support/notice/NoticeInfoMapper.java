51e211e70d9d4b882c95b9db8358691feef863ea
kimhoyoun <hoyoun1309@gmail.com>
2022-07-26 23:21:14 +0900

    페이징 완료

--- src/main/java/org/com/proj/support/notice/NoticeInfoMapper.java
+++ src/main/java/org/com/proj/support/notice/NoticeInfoMapper.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public interface NoticeInfoMapper {
 
+	// 전체
 	public List<NoticeDTO> selectAll(HashMap<String, Object> map); 
-	
+	// 상세
 	public NoticeDTO selectOne(String id); 
-	
+	// 입력
 	public void insert(NoticeDTO dto);
-	
+	// 페이징
 	public Long selectBoardCount();
 	
 }
